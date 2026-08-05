58c606c71a8c58370cdb5f3486e8fcc7bb440d23
Raphael <rnga3672@gmail.com>
2026-08-05 16:02:58 -0400

    working script

---
+++ main.py
@@ -0,0 +1,70 @@
+import asyncio
+
+
+from twikit import Client #pip install git+https://github.com/PawiX25/twifork.git
+from dotenv import load_dotenv
+import yt_dlp
+import os
+from time import sleep
+
+
+load_dotenv()
+USERNAME = os.environ['USERNAME_LOGIN']
+EMAIL = os.environ['EMAIL_LOGIN']
+PASSWORD = os.environ['PASS_LOGIN']
+tweetsToCheck = 15
+# Initialize client
+client = Client('en-US', impersonate='chrome124')
+
+async def main():
+    client.load_cookies(os.environ['COOKIE_FILE_JSON'])
+
+    await client.is_logged_in()
+
+    to_download = []
+
+    user = await client.get_user_by_screen_name('isitmeagainwow')
+    print(user)
+    tweets = []
+    _tweets = await client.get_user_tweets(user.id, 'Tweets', count=15)
+    for tweet in _tweets:
+        if (tweet.retweeted_tweet):
+            tweets.append(tweet.retweeted_tweet)
+
+    
+    while len(tweets) < tweetsToCheck:
+        sleep(3)
+        print(f"Fetching new tweets. Currently at {len(tweets)}.")
+        _tweets = await _tweets.next()
+        for tweet in _tweets:
+            if tweet.retweeted_tweet:
+                tweets.append(tweet.retweeted_tweet)
+        print(f"Fetched new tweets: {len(tweets)} tweets total.")
+
+
+    for tweet in tweets:
+        if tweet.media:
+            for file in tweet.media:
+                to_download.append(file.url)
+    
+    i = 0
+    print(f"{len(to_download)} medias available for downloading.")
+    for fileurl in to_download:
+        i = i + 1
+        print(f"{i}/{len(to_download)}")
+        ydl_opts = {
+            'cookiefile':os.environ['COOKIE_FILE_TXT'],
+            "format": "bv*+ba/best",
+            "merge_output_format": "mp4",
+            "prefer_free_formats": False,
+            "ignoreerrors": True,
+            "nooverwrites": True,
+            "windowsfilenames": True,
+            "restrictfilenames": True,
+            "outtmpl": os.environ['OUTPUT_MPL']
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([fileurl])
+
+
+asyncio.run(main())
